2d24b7699abd31f65fce8ee0f6001208c2a93706
ShernShiou <shernshiou@gmail.com>
2014-03-26 20:59:03 +0800

    Credit card accept amount

--- Week7/Question1/CreditCardPayment.java
+++ Week7/Question1/CreditCardPayment.java
@@ -9,6 +9,13 @@ public class CreditCardPayment extends Payment {
     this.expiration = expiration;
   }
 
+  public CreditCardPayment(String cardName, String cardNumber, String expiration, double amount) {
+    super(amount);
+    this.cardName = cardName;
+    this.cardNumber = cardNumber;
+    this.expiration = expiration;
+  }
+
   @Override public String paymentDetails() {
     return String.format(
       "The amount is %.2f (credit card).\n Name: %s \n Number: %s \n Expiration Date: %s \n"
--- Week7/Question1/Question1.java
+++ Week7/Question1/Question1.java
@@ -3,7 +3,7 @@ public class Question1 {
     CashPayment cash1 = new CashPayment();
     CashPayment cash2 = new CashPayment(28.30);
     CreditCardPayment creditCard1 = new CreditCardPayment("Ethelyn Debusk", "4111111111111111", "2014-04-21");
-    CreditCardPayment creditCard2 = new CreditCardPayment("Kurtis Vidaurri", "4012888888881881", "2014-09-05");
+    CreditCardPayment creditCard2 = new CreditCardPayment("Kurtis Vidaurri", "4012888888881881", "2014-09-05", 999.02);
 
     System.out.println(cash1.paymentDetails());
     System.out.println(cash2.paymentDetails());
